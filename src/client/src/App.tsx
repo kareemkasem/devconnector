65d62d5f3be5fc773f02521b410a364ccf4d62f1
kareem kasem <kareemkasem@outlook.com>
2020-10-29 22:00:43 +0200

    added react router use links added boilerplate for login/signup/login

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,13 +1,25 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 import Navbar from "./components/layouts/navbar";
 import Landing from "./components/layouts/landing";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import Profiles from "./components/profiles";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Landing />
+      <section className="container">
+        <Switch>
+          <Route path="/" exact component={Landing} />
+          <Route path="/signup" exact component={Signup} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/profiles" exact component={Profiles} />
+        </Switch>
+      </section>
     </>
   );
 };
