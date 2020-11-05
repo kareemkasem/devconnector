93f51a8d4143792c67973e78d4f3c22c0339352a
kareem kasem <kareemkasem@outlook.com>
2020-11-05 16:40:41 +0200

    minor bug fix

--- src/client/src/store/actions/action.types.ts
+++ src/client/src/store/actions/action.types.ts
@@ -3,7 +3,7 @@ import { AlertType } from "../types";
 export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
 
-export type SetAlerttype = {
+export type SetAlertType = {
   type: typeof SET_ALERT;
   payload: AlertType;
 };
@@ -14,4 +14,4 @@ export type RemoveAlertType = {
   };
 };
 
-export type AppActionTypes = SetAlerttype | RemoveAlertType;
+export type AppActionTypes = SetAlertType | RemoveAlertType;
--- src/client/src/store/actions/alerts.ts
+++ src/client/src/store/actions/alerts.ts
@@ -1,9 +1,9 @@
 import { v4 as uuid } from "uuid";
 
 import { SET_ALERT, REMOVE_ALERT } from "./action.types";
-import { SetAlerttype, RemoveAlertType } from "./action.types";
+import { SetAlertType, RemoveAlertType } from "./action.types";
 
-export const setAlert = (msg: string, alertType: string): SetAlerttype => ({
+export const setAlert = (msg: string, alertType: string): SetAlertType => ({
   type: SET_ALERT,
   payload: {
     id: uuid(),
