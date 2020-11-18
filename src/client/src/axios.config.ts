9d7c2411df36d7151a0794edf83bc3a3260759c3
kareem kasem <kareemkasem@outlook.com>
2020-11-18 19:02:55 +0200

    axiosConfig update

--- src/client/src/axios.config.ts
+++ src/client/src/axios.config.ts
@@ -1,12 +1,18 @@
 import Axios, { AxiosInstance } from "axios";
 export default class AxiosConfig {
+  constructor() {
+    this.configureDefaults();
+  }
+
   private token: string | null = localStorage.getItem("token");
 
   public instance: AxiosInstance = Axios.create({
     baseURL: process.env.REACT_APP_BASEURL,
-    headers: {
-      "Content-Type": "application/json",
-      "x-auth-token": this.token,
-    },
   });
+
+  private configureDefaults() {
+    this.instance.defaults.headers.common["Content-Type"] = "application/json";
+    if (this.token)
+      this.instance.defaults.headers.common["x-auth-token"] = this.token;
+  }
 }
