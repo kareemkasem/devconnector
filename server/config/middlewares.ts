f7a4b4253fa8642282eda4fe0ab7189eb85dadaf
kareem kasem <kareemkasem@outlook.com>
2020-12-11 13:10:11 +0200

    fixed outdir for tsc

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -3,6 +3,5 @@ import cors from "cors";
 
 export default app => {
   app.use(express.json());
-  app.options("*", cors());
-  app.use(cors({ origin: process.env.CLIENT_ORIGIN }));
+  app.use(cors());
 };
