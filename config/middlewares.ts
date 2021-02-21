cd2e9d2286ab375cb3e3645011fb12d97b9cc310
kareem kasem <kareemkasem@outlook.com>
2021-02-22 00:16:53 +0200

    I'm LITTERLY copy pasting at this point

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,15 +1,7 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
 
 export default app => {
 	app.use(express.json());
 	app.use(cors());
-
-	if (process.env.NODE_ENV === "production") {
-		app.use(express.static("../client/build"));
-		app.get("*", (req, res) => {
-			res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
-		});
-	}
 };
