9a6134fd768a52ebbfdd0f1d56aa1ae5de517ab5
kareem kasem <kareemkasem@outlook.com>
2021-02-21 23:40:14 +0200

    :')

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -7,11 +7,9 @@ export default app => {
 	app.use(cors());
 
 	if (process.env.NODE_ENV === "production") {
-		app.use(express.static("../client/build"));
+		app.use(express.static("./client/build"));
 		app.get("*", (req, res) => {
-			res.sendFile(
-				path.resolve(__dirname, "..", "client", "build", "index.html")
-			);
+			res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 		});
 	}
 };
